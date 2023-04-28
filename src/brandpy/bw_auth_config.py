b88c510c0b641c84290d4d95a768b638e5901673
deamonpog <pog666@gmail.com>
2023-04-28 04:52:08 -0400

    revised how auth works and how new tokens are requested to support any architecture

--- src/brandpy/bw_auth_config.py
+++ src/brandpy/bw_auth_config.py
@@ -5,14 +5,39 @@ import json
 import time
 import getpass
 import requests
-import pandas as pd
 
-import pathlib
-import typing
+
+# import pathlib
+# import typing
 
 
 class BWAuthConfig:
-    def __init__(self, in_token_file_path, in_queue_file_path=None, in_prompt_if_failed=True):
+
+    @staticmethod
+    def request_new_token(in_email, in_pwd):
+        url = f'https://api.brandwatch.com/oauth/token?username={in_email}&grant_type=api-password&client_id=brandwatch-api-client'
+        response = requests.request("POST", url, params={"password": in_pwd})
+        response.raise_for_status()
+        # if 200 != response.status_code:
+        #     print("Failed to get API token.")
+        #     print(response)
+        #     print(response.json())
+        #     raise Exception("Failed to get API token. {}".format(response.json()))
+        # else:
+        #     print("Successfully got the API token.")
+        return response.json()
+
+    @staticmethod
+    def save_token_data_to_file(in_email, in_token_file_path, in_queue_file_path, in_token_json):
+        in_token_json["e-mail"] = in_email
+        in_token_json["expire_on"] = in_token_json['expires_in'] + time.time()
+        in_token_json["queue_file"] = in_queue_file_path
+        with open(in_token_file_path, 'w', encoding="utf-8") as fout:
+            fout.write(json.dumps(in_token_json))
+            expiry_date = datetime.datetime.fromtimestamp(in_token_json["expire_on"])
+            print("Token Saved. Usable untill : {}.".format(expiry_date))
+
+    def __init__(self, in_token_file_path, in_queue_file_path=None, in_prompt_if_failed=False):
         """
         Either reads the token from existing file location or requests a new token from Brandwatch and saves the token to the given file path.
 
@@ -26,8 +51,11 @@ class BWAuthConfig:
         :type in_queue_file_path: str
         """
         # create token file if not exist
+        self.token = None
+        self.email = None
         if in_queue_file_path is None:
-            self.queue_file_path = os.path.abspath(os.path.join(os.path.dirname(in_token_file_path), "BWQueryQueue.csv"))
+            self.queue_file_path = os.path.abspath(
+                os.path.join(os.path.dirname(in_token_file_path), "BWQueryQueue.csv"))
         else:
             self.queue_file_path = in_queue_file_path
         self.token_file_path = in_token_file_path
@@ -35,44 +63,27 @@ class BWAuthConfig:
             print("Token file does not exist.")
             if in_prompt_if_failed:
                 print("Retrieving token from Brandwatch API...")
-                response = self.__get_token()
-                self.__save_token(response)
+                response_json = self.prompt_and_get_token()
+                BWAuthConfig.save_token_data_to_file(self.email, self.token_file_path, self.queue_file_path, response_json)
             else:
                 raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.token_file_path)
         # read token from file
-        self.__read_token()
+        self.read_token_data_from_file()
 
-    def __save_token(self, in_response):
-        if 200 == in_response.status_code:
-            print("Successfully got the API token.")
-            token_data = in_response.json()
-            token_data["e-mail"] = self.email
-            token_data["expire_on"] = token_data['expires_in'] + time.time()
-            token_data["queue_file"] = self.queue_file_path
-            with open(self.token_file_path, 'w', encoding="utf-8") as fout:
-                fout.write(json.dumps(token_data))
-                expdate = datetime.datetime.fromtimestamp(token_data["expire_on"])
-                print("Token Saved. Usable untill : {}.".format(expdate))
-        else:
-            print("Failed to get API token.")
-            print(in_response)
-            print(in_response.json())
-            raise Exception("Failed to get API token. {}".format(in_response.json()))
-
-    def __read_token(self):
+    def read_token_data_from_file(self):
         print("Reading form Token file...")
         with open(self.token_file_path, encoding="utf-8") as fin:
-            jsdata = json.load(fin)
-        self.email = jsdata["e-mail"]
-        token = jsdata["access_token"]
-        expiry = jsdata["expire_on"]
+            user_token_data = json.load(fin)
+        self.email = user_token_data["e-mail"]
+        token = user_token_data["access_token"]
+        expiry = user_token_data["expire_on"]
         # make sure it doesn't expire for at least an hour
         print(f"Token belongs to : {self.email}")
         print("Token Expires by : {}".format(datetime.datetime.fromtimestamp(expiry)))
-        self.token = jsdata["access_token"]
-        self.queue_file_path = jsdata["queue_file"]
+        self.token = user_token_data["access_token"]
+        self.queue_file_path = user_token_data["queue_file"]
 
-    def __get_token(self):
+    def prompt_and_get_token(self):
         """
         Prompts the user to enter username and password for retrieving the token from Brandwatch API.
         :return:
@@ -80,7 +91,6 @@ class BWAuthConfig:
         """
         self.email = input("Enter your username: ")
         pwd = getpass.getpass(prompt="Enter password (program doesn't save this): ")
-        url = f'https://api.brandwatch.com/oauth/token?username={self.email}&grant_type=api-password&client_id=brandwatch-api-client'
-        response = requests.request("POST", url, params={"password": pwd})
+        response_json = BWAuthConfig.request_new_token(self.email, pwd)
         del pwd
-        return response
+        return response_json
